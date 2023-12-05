fc35eb49f333ff32f5f0defc33bea72571800bbb
leobewater <mmbee888@gmail.com>
2023-12-05 13:06:32 -0500

    add flash message after project created or updated

--- backend/controllers/ProjectController.php
+++ backend/controllers/ProjectController.php
@@ -4,6 +4,7 @@ namespace backend\controllers;
 
 use common\models\Project;
 use backend\models\ProjectSearch;
+use Yii;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -71,6 +72,8 @@ class ProjectController extends Controller
 
         if ($this->request->isPost) {
             if ($model->load($this->request->post()) && $model->save()) {
+                // flash message
+                Yii::$app->session->setFlash('success', 'Successfully saved');
                 return $this->redirect(['view', 'id' => $model->id]);
             }
         } else {
@@ -94,6 +97,8 @@ class ProjectController extends Controller
         $model = $this->findModel($id);
 
         if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            // flash message
+            Yii::$app->session->setFlash('success', 'Successfully updated');
             return $this->redirect(['view', 'id' => $model->id]);
         }
 
