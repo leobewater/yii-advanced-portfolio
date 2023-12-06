245c723d9fffb7200951bfc0c8bc06893df923f2
leobewater <mmbee888@gmail.com>
2023-12-06 16:18:14 -0500

    show rating star plugin

--- backend/views/testimonial/_form.php
+++ backend/views/testimonial/_form.php
@@ -1,6 +1,7 @@
 <?php
 
 use kartik\file\FileInput;
+use kartik\rating\StarRating;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\widgets\ActiveForm;
@@ -17,12 +18,12 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'project_id')->dropDownList($projects, ['prompt' => Yii::t('app', 'Select...')]) ?>
 
-    <?php // using 3rd party plugin https://demos.krajee.com/widget-details/fileinput ?>
+    <?php // using 3rd party plugin https://demos.krajee.com/widget-details/fileinput?>
     <?= $form->field($model, 'imageFile')->widget(FileInput::class, [
       'options' => ['accept' => 'image/*' ],
       'pluginOptions' => [
         'initialPreview' => $model->imageAbsoluteUrl(),
-        'initialPreviewAsData'=>true,
+        'initialPreviewAsData' => true,
         'showUpload' => false,
         'initialPreviewConfig' => $model->imageConfig(),
         'deleteUrl' => Url::to(['testimonial/delete-customer-image'])
@@ -37,7 +38,11 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'review')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'rating')->textInput() ?>
+    <!-- <?= $form->field($model, 'rating')->textInput() ?> -->
+
+    <?= $form->field($model, 'rating')->widget(StarRating::class, [
+        'pluginOptions' => ['step' => 1]
+    ]); ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
