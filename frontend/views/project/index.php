f4e3925b4164625da08b07eb1a7ffaa208812b1b
leobewater <mmbee888@gmail.com>
2023-12-07 10:21:01 -0500

    remove FE search and create components

--- frontend/views/project/index.php
+++ frontend/views/project/index.php
@@ -17,12 +17,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Project'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
-
     <?= ListView::widget([
         'dataProvider' => $dataProvider,
         'itemOptions' => ['class' => 'item'],
