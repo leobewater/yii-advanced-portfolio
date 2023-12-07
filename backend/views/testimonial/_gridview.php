cd824c29c81ce5c6b810641d26607fe279e6a6c4
leobewater <mmbee888@gmail.com>
2023-12-07 09:01:41 -0500

    extract testimonial to own file

---
+++ backend/views/testimonial/_gridview.php
@@ -0,0 +1,61 @@
+<?php // echo $this->render('_search', ['model' => $searchModel]); 
+
+use common\models\Testimonial;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var backend\models\TestimonialSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+/** @var common\models\Project[] $projects */
+?>
+
+<?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+        // ['class' => 'yii\grid\SerialColumn'],
+        'id',
+        [
+          // output project name instead of id
+          'attribute' => 'project_id',
+          'format' => 'raw',
+          'filter' => $projects,
+          'value' => function($model) {
+            /**
+             * @var $model common\models\Testimonial
+             */
+            return Html::a($model->project->name, ['project/view', 'id' => $model->project_id]);
+          }
+        ],
+        [
+          // output image instead of image id
+          'attribute' => 'customer_image_id',
+          'format' => 'raw',
+          'value' => function($model) {
+            /**
+             * @var $model common\models\Testimonial
+             */
+            if(!$model->customerImage) {
+              return null;
+            }
+            return Html::img($model->customerImage->absoluteUrl(), [
+              'alt' => $model->customer_name,
+              'height' => 75,
+            ]);
+          }
+        ],
+        'title',
+        'customer_name',
+        //'review:ntext',
+        'rating',
+        [
+            'class' => ActionColumn::class,
+            'urlCreator' => function ($action, Testimonial $model, $key, $index, $column) {
+                return Url::toRoute([$action, 'id' => $model->id]);
+             }
+        ],
+    ],
+]); ?>
--- backend/views/testimonial/index.php
+++ backend/views/testimonial/index.php
@@ -22,55 +22,10 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a(Yii::t('app', 'Create Testimonial'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            // ['class' => 'yii\grid\SerialColumn'],
-            'id',
-            [
-              // output project name instead of id
-              'attribute' => 'project_id',
-              'format' => 'raw',
-              'filter' => $projects,
-              'value' => function($model) {
-                /**
-                 * @var $model common\models\Testimonial
-                 */
-                return Html::a($model->project->name, ['project/view', 'id' => $model->project_id]);
-              }
-            ],
-            [
-              // output image instead of image id
-              'attribute' => 'customer_image_id',
-              'format' => 'raw',
-              'value' => function($model) {
-                /**
-                 * @var $model common\models\Testimonial
-                 */
-                if(!$model->customerImage) {
-                  return null;
-                }
-                return Html::img($model->customerImage->absoluteUrl(), [
-                  'alt' => $model->customer_name,
-                  'height' => 75,
-                ]);
-              }
-            ],
-            'title',
-            'customer_name',
-            //'review:ntext',
-            'rating',
-            [
-                'class' => ActionColumn::class,
-                'urlCreator' => function ($action, Testimonial $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
-        ],
+    <?= $this->render('_gridview', [
+      'searchModel' => $searchModel,
+      'dataProvider' =>  $dataProvider,
+      'projects' => $projects
     ]); ?>
-
 
 </div>
