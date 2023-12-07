df14e7c6c257bcaea25b187bc7cf18383ad5bc8c
leobewater <mmbee888@gmail.com>
2023-12-07 10:19:58 -0500

    generate project view and list views

---
+++ frontend/views/project/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use common\models\Project;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\widgets\ListView;
+
+/** @var yii\web\View $this */
+/** @var frontend\models\ProjectSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = Yii::t('app', 'Projects');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="project-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Project'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'itemView' => function ($model, $key, $index, $widget) {
+            return Html::a(Html::encode($model->name), ['view', 'id' => $model->id]);
+        },
+    ]) ?>
+
+
+</div>
