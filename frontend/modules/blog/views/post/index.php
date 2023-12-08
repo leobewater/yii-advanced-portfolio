ea41ca72ac2e37b81f3d5d15bddfc3315e8c20e7
leobewater <mmbee888@gmail.com>
2023-12-08 09:40:30 -0500

    update post index page

--- frontend/modules/blog/views/post/index.php
+++ frontend/modules/blog/views/post/index.php
@@ -17,15 +17,10 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Post'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
-
     <?= ListView::widget([
         'dataProvider' => $dataProvider,
-        'itemOptions' => ['class' => 'item'],
+        'itemOptions' => ['class' => 'post'],
+        'layout' => "{items}\n{pager}",
         'itemView' => function ($model, $key, $index, $widget) {
             return Html::a(Html::encode($model->title), ['view', 'id' => $model->id]);
         },
