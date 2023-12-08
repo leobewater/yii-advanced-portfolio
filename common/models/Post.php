9a5713716bd89347756aa111930b50a87dc25095
leobewater <mmbee888@gmail.com>
2023-12-08 08:10:42 -0500

    use title to create post slug from behaviors method

--- common/models/Post.php
+++ common/models/Post.php
@@ -3,6 +3,8 @@
 namespace common\models;
 
 use Yii;
+use yii\behaviors\SluggableBehavior;
+use yii\behaviors\TimestampBehavior;
 
 /**
  * This is the model class for table "post".
@@ -31,7 +33,7 @@ class Post extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['title', 'body', 'slug', 'is_published', 'created_at', 'updated_at'], 'required'],
+            [['title', 'body', 'is_published'], 'required'],
             [['body', 'slug'], 'string'],
             [['is_published', 'created_at', 'updated_at'], 'integer'],
             [['title'], 'string', 'max' => 255],
@@ -52,6 +54,19 @@ class Post extends \yii\db\ActiveRecord
             'created_at' => Yii::t('app', 'Created At'),
             'updated_at' => Yii::t('app', 'Updated At'),
         ];
+    }
+
+    // handle slug creation
+    public function behaviors()
+    {
+      return [
+        TimestampBehavior::class,
+        [
+          'class' => SluggableBehavior::class,
+          'attribute' => 'title', // use title to create slug and make sure it's unique
+          'ensureUnique' => true,
+        ],
+      ];
     }
 
     /**
