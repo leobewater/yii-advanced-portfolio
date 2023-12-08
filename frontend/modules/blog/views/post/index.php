068906a96549f1a99ea5783a8aec7dd367ba54eb
leobewater <mmbee888@gmail.com>
2023-12-08 09:31:24 -0500

    generate FE post views

---
+++ frontend/modules/blog/views/post/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use common\models\Post;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\widgets\ListView;
+
+/** @var yii\web\View $this */
+/** @var frontend\modules\blog\models\PostSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = Yii::t('app', 'Posts');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="post-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Post'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'itemView' => function ($model, $key, $index, $widget) {
+            return Html::a(Html::encode($model->title), ['view', 'id' => $model->id]);
+        },
+    ]) ?>
+
+
+</div>
