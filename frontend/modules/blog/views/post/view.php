03e5409944a2c41a614e10c366e8c3c5885db32b
leobewater <mmbee888@gmail.com>
2023-12-08 09:43:20 -0500

    customize post detail page

--- frontend/modules/blog/views/post/view.php
+++ frontend/modules/blog/views/post/view.php
@@ -15,28 +15,11 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <div class="post-view__date">
+      <?= "Created At: " . Yii::$app->formatter->asDate($model->created_at) ?>
+    </div>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'title',
-            'body:ntext',
-            'slug:ntext',
-            'is_published',
-            'created_at',
-            'updated_at',
-        ],
-    ]) ?>
-
+    <div class="post-view__body">
+      <?= Yii::$app->formatter->asParagraphs($model->body) ?>
+    </div>
 </div>
