efd1529cdccc9442e55219bc3902430dc424e956
leobewater <mmbee888@gmail.com>
2023-12-08 08:00:35 -0500

    generate Post CRUD in backend blog module

---
+++ backend/modules/blog/views/post/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var common\models\Post $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="post-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'slug')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'is_published')->textInput() ?>
+
+    <?= $form->field($model, 'created_at')->textInput() ?>
+
+    <?= $form->field($model, 'updated_at')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
